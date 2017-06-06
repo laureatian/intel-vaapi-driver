a4bdd529e244c8cf8d2c67bd8c0098c97caf459a
Wang Tiantian <tiantian.wang@intel.com>
2017-06-06 10:09:49 +0800

    set all layer id = 0

--- src/i965_encoder.c
+++ src/i965_encoder.c
@@ -757,14 +757,15 @@ intel_encoder_check_temporal_layer_structure(VADriverContextP ctx,
     int i;
 
     if (!encoder_context->is_new_sequence) {
-        if (encoder_context->layer.num_layers > 1)
-            encoder_context->layer.curr_frame_layer_id = encoder_context->layer.frame_layer_ids[(encoder_context->num_frames_in_sequence - 1) % encoder_context->layer.size_frame_layer_ids];
-        else
-            encoder_context->layer.curr_frame_layer_id = 0;
+        /*        if (encoder_context->layer.num_layers > 1)
+                    encoder_context->layer.curr_frame_layer_id = encoder_context->layer.frame_layer_ids[(encoder_context->num_frames_in_sequence - 1) % encoder_context->layer.size_frame_layer_ids];
+                else */
+        encoder_context->layer.curr_frame_layer_id = 0;
         printf("num_fame_in sequence:%d\n", encoder_context->num_frames_in_sequence);
         return VA_STATUS_SUCCESS;
     }
 
+    encoder_context->layer.curr_frame_layer_id = 0;
     if (!(rate_control_mode & (VA_RC_CBR | VA_RC_VBR)))
         return VA_STATUS_SUCCESS;
 
